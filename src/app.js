51e897cac5006f4019de28031d74467fc03604a7
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-05-03 19:17:41 +0530

    Build

--- src/app.js
+++ src/app.js
@@ -10,5 +10,22 @@ app.use(express.json());
 
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
+const api2 = async (req, res) => {
+  try {
+    [...products].forEach((item) => {
+      if (parseInt(req.params.id) === item.id) {
+        res.status(200).json({
+          status: "success",
+          message: "Product fetched successfully",
+          data: { product: { ...item } },
+        });
+        return;
+      }
+    });
+  } catch (error) {
+    res.status(404).json({ message: "Product not found", error });
+  }
+};
+app.get("/products/:id", api2);
 
 module.exports = app;
